d8a92cc0a5080630aed565cb189d9617afa5ab84
likhithih <likhithih2003@gmail.com>
2025-11-08 22:43:17 +0530

    test1

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const dotenv = require('dotenv');
-import MONGO_URl from process.env.MONGO_URl;
+const MONGO_URI = process.env.MONGO_URI;
 
 
 dotenv.config();
