1e11e866cdecf804c3da831a0c6b8c70f3cd0420
likhithih <likhithih2003@gmail.com>
2025-11-09 06:43:47 +0530

    updates

--- backend/server.js
+++ backend/server.js
@@ -26,10 +26,7 @@ app.use(cors());
 app.use(express.json());
 
 // MongoDB connection
-mongoose.connect(MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-})
+mongoose.connect(MONGO_URI)
 .then(() => console.log('MongoDB connected'))
 .catch(err => console.log(err));
 
