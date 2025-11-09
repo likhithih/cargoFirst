2361da6b0f7598306cefbd66559c268c252364a3
likhithih <likhithih2003@gmail.com>
2025-11-09 06:52:41 +0530

    revet

--- backend/server.js
+++ backend/server.js
@@ -27,8 +27,6 @@ app.use(express.json());
 
 // MongoDB connection
 mongoose.connect(MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
 })
 .then(() => console.log('MongoDB connected'))
 .catch(err => console.log(err));
