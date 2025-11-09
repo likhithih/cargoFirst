8f4fa1955844256edc5a6400f4b563e4776a2272
likhithih <likhithih2003@gmail.com>
2025-11-09 06:47:46 +0530

    revert the changes

--- backend/server.js
+++ backend/server.js
@@ -26,7 +26,10 @@ app.use(cors());
 app.use(express.json());
 
 // MongoDB connection
-mongoose.connect(MONGO_URI)
+mongoose.connect(MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
 .then(() => console.log('MongoDB connected'))
 .catch(err => console.log(err));
 
